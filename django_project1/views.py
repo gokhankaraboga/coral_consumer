65527b6c59a054f5acdb89241e83ebd24d5be0ca
Gokhan Karaboga <gokhankaraboga@Gokhans-MacBook-Air.local>
2016-11-02 14:21:41 +0300

    ++

--- django_project1/views.py
+++ django_project1/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-
-def homepage(request):
-    return HttpResponse('Homepage')
